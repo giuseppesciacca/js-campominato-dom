b1766ed52169d69076be31f3abe7f81a71563202
giuseppesciacca <sciaccagi@gmail.com>
2023-03-20 13:36:41 +0100

    [add] console.log sum points

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 /*  Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-
 In seguito l'utente clicca su una cella:
 se il numero è presente nella lista dei numeri generati abbiamo calpestato una bomba la cella si colora di rosso e la partita termina.
 Altrimenti la cella cliccata si colora di azzurro l'utente può continuare a cliccare sulle altre celle.
+
 La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */
 
@@ -11,6 +11,7 @@ const rowEl = document.getElementById('row_main');
 const btn_play = document.getElementById('btn_play');
 const difficultyEl = document.getElementById('difficulty');
 const verdictEl = document.getElementById('verdict');
+let points = 0;
 
 btn_play.addEventListener('click', function () {
     rowEl.innerHTML = '';   //svuoto il precedente campo
@@ -61,9 +62,12 @@ function campoMinato(numCells, array) {
 //*************
 function colorSquare(element, index, array) {
     element.addEventListener('click', function () {
-        this.classList.toggle('active');
+        this.classList.add('active');
         console.log(`Hai cliccato la cella numero ${index + 1}`);
         this.innerHTML = `<span> ${index + 1} </span>`;
+
+        points += (index + 1)
+        console.log('point ' + points);
 
         detectionBomb(index, array, element)
     })
@@ -95,7 +99,8 @@ function detectionBomb(i, array, squareElement) {
 
         if (i == element) {
             squareElement.classList.add('bomb');
-            verdictEl.classList.remove('d-none');
+            squareElement.innerHTML = `<i class="fa-solid fa-bomb fa-4x"></i>`
+            /* verdictEl.classList.remove('d-none'); */
         }
     }
 }
